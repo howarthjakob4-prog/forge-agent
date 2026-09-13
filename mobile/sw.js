a9b4bbd84d7fc150a56fd555ad410d81dd278bca
Jakob Howarth <howarthjakob4@gmail.com>
2026-09-12 23:43:55 -0400

    Add mobile offline shell service worker

---
+++ mobile/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'forge-mobile-v1';
+const ASSETS = ['/', '/index.html', '/app.css', '/app.js', '/manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith('/api/')) return;
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+      return response;
+    }).catch(() => caches.match(event.request))
+  );
+});
